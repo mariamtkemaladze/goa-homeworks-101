9b9c398ba999d106e3fdbf5680a4fda30145d4ec
mariamtkemaladze <mariamtkemaladze1234@gmail.com>
2026-04-29 21:47:05 +0400

    first commit

---
+++ level 3/class/classwork.py
@@ -0,0 +1,19 @@
+#compilation - kodis gardaqmna da gashveba
+#compiler - garemo sadac vushvebt kods
+
+print(5 + 7)
+print( 10 - 6 )
+print( 8 / 4 )
+print( 8 // 4 )
+print(12 * 4 )
+print( 44 % 13 )
+
+
+name = "mari"
+age = "16"
+fav_color = "black and red"
+fav_car = "subaru forester"
+city = "tbilisi"
+
+
+print(f"hi, my name is {name}. i'm {age} years old and i live in {city}. my fav colors are {fav_color} and i love {fav_car} so much.")
---
+++ level 3/home/homework.py
@@ -0,0 +1,19 @@
+#კოდის წერის პროცესი შედგება 3 ნაწილისგან writing, running, debugging
+# / გამოაქვს გაყოფის შედეგად მიღებული რიცხვი ათწილადად. //-ს გამოაქვს შედეგი დამრგვალებულად, მთელ რიცხვში.
+#case-sensitive - პროგრამა რომელიც განსხვავებულად ღიქვამს cap ასიებს და პატარა ასოებს, case-insensitive კი ერთნაირ ასოებად თვლის მათ. 
+
+name = "dato"
+nickname = "chill guy"
+word = "arafeli"
+
+
+print(f"he's name is {name} and he's nickname is {nickname}. he can't pronounce the word 'araferi' and pronounces it as {word}")
+
+num1 = 34
+num2 = 54
+num3 = 67
+
+print(num1 + num3)
+print(num3 - num1)
+print(num2 // num1)
+print(num2 * num3)
