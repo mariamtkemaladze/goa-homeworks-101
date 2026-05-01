3119131c501f35c28a944ef7cf06ca557c4c88ae
mariamtkemaladze <mariamtkemaladze1234@gmail.com>
2026-05-01 21:45:44 +0400

    second commit

---
+++ level 4/class/classwork.py
@@ -0,0 +1,18 @@
+name = input("enter your nanme:/n")
+print(name)
+
+name2 = input("enter your name:")
+last_name = input("enter your last name:")
+age = input("enter your age:")
+city = input("enter your city:")
+country = input("enter your country:")
+
+print(f"hello, my name is {name2} {last_name}, i am {age} years old and i live in {country}, {city}.")
+
+string = "dato is black"
+integer = 24
+float = 0.5
+
+print(type(string))
+print(type(integer))
+print(type(float))
---
+++ level 4/home/homework.py
@@ -0,0 +1,17 @@
+#input - racxa shemavali info, output- racxa gamomavali info
+#snake_case-s stili- cvladis sheqmnis dros qvedatires gamoyeneba aris amis magaliti
+
+meow = input()
+print(type(meow))
+
+name = "mari"   #str
+age = 16         #int
+birthdate = 8.03   #float
+favcolor = "idk"  #str
+band = "xdiz"  #str
+
+
+name = input("enter your name:")
+age = input("enter your age:")
+
+print(name + age)
