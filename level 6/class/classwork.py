7706a6c4c779cdefed5f20c45afb7b5adf9dc2c3
mariamtkemaladze <mariamtkemaladze1234@gmail.com>
2026-05-08 21:31:51 +0400

    second commit

---
+++ level 6/class/classwork.py
@@ -0,0 +1,25 @@
+x = input(int("enter x number:"))
+y = input(int("enter y number:"))
+
+print(x + y)
+
+age = input(int("enter your age:"))
+
+print(age + 15)
+
+print(10 > 5 )
+print(15 < 3 )
+print(15 == 56)
+print(6 <= 6)
+print(12 >= 34)
+
+print(False and False)
+print(True and True)
+print(False and True)
+print(True and False)
+
+print(True or True)
+print(False or False)
+print(False or True)
+print(True or False)
+
---
+++ level 6/home/homework.py
@@ -0,0 +1,19 @@
+print(10 > 5)
+print(24 < 45)
+print(10 == 10)
+print(67 <= 34)
+print(23 >= 54)
+
+#logical operators - and / or
+#comparison operators - >,<,==,<=,>=
+
+num = input(int("enter num:"))
+print(num > 5)
+
+name = input("enter your name:")
+
+print(name == "mary")
+
+
+age = input(int("enter your age:"))
+print(age > 18)
