ce25252b5404c04f09da2c19e02345f9d7186bc7
mariamtkemaladze <mariamtkemaladze1234@gmail.com>
2026-05-15 21:32:32 +0400

    miau

---
+++ level 8/home/homework.py
@@ -0,0 +1,26 @@
+print(5<10)
+print(8>6)
+print(4<78)
+print(4>1)
+print(45>23)
+
+print(23>45)
+print(89<2344)
+print(34<23)
+print(2>3)
+print(89<67)
+
+#for loop gvexmareba davprintot mnishvneloba imdenjer ramdenjerac gvinda
+#range-s gadaecema start end da step, axsna mezareba
+
+for i in range(1,11):
+    print("subaru forester")
+
+for i in range(1,101):
+    print("tkemala")   
+
+for i in range(1,47):
+    print("i dont have fav color")
+
+for i in range(1,33):
+    print("m")
