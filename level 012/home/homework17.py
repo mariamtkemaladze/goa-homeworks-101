7adec14490e8e82a308e70f69a79b6b1dbb98c19
mariamtkemaladze <mariamtkemaladze1234@gmail.com>
2026-06-05 22:06:47 +0400

    miau

--- level 012/home/homework17.py
+++ level 012/home/homework17.py
@@ -2,6 +2,6 @@ num = int(input("enter number:"))
 
 sum = 0
 for i in range(1, num + 1):
-    sum += i         #es xazi vegar gavige, rogor amatebs ertmanets?
+    sum += i        # anu tu aris 2 mere amatebs 3s da tu 3ia mashin 4s da a.sh.
 
 print(sum)
---
+++ level 014/class/classwork.py
@@ -0,0 +1,13 @@
+arr = ["subaru", "nissan", "porsche", "mercedes", "mazda"]
+
+print(arr[0])
+print(arr[2])
+
+
+list = [2, 4, 6, 8, 10]
+print(list[-1])
+print(list[-3])
+
+
+frnds = ["dato", "ele", "gio", "lasha", "nini"]
+print(frnds[3])
