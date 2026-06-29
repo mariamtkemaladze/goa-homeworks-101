186025a8e092d6ae12c9b6f75a9d096c01433255
mariamtkemaladze <mariamtkemaladze1234@gmail.com>
2026-06-29 19:20:09 +0400

    meow

---
+++ level 020/home/homework.py
@@ -0,0 +1,39 @@
+#upper() funqcias stringis yvela aso magal registrshi gadaaqvs.
+#lower() funqcias piriqit dabal registrshi.
+#capitalize() funqcias mxolod sityvebis pirveli asoebi gadayavs magal registrshi.
+#find() funqcia poulobs mititebuli asos indexs.
+#count() funqcias sheudzlia daitvalos ramdenjer aris mititebuli aso gamoyenebuli stringshi.
+#len() funqcia itvlis siis, an nebismieri mnishvnelobis sigrdzes.
+#endswith() funqcia amowmebs mtavrdeba tuara konkretuli mnishvnelobis daboloeba mititebuli stringit.
+#startswith() amowmebs iwyeba tuara konkretuli mnishvneloba mititebuli stringit.
+
+
+# word = input("enter a word:")
+# print(word.lower())
+
+
+# email = input("enter your email:")
+# result = email.find("@") != -1
+
+# print(str(result).upper())
+
+
+# book = input("enter book title:")
+# print(book.capitalize())
+
+
+# word = input("enter a word:")
+# symbol = input("enter a symbol")
+
+# print(word.count(symbol))
+
+
+
+# word = input("enter a word:")
+
+# word2 = word.upper()
+
+# if word2 == word:
+#     print("word is already w big letters!")
+# else:
+#     print(word.upper())
