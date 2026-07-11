cc0ccffe530db24eca9f99309b38a38192bf4a0d
mariamtkemaladze <mariamtkemaladze1234@gmail.com>
2026-07-11 21:21:41 +0400

    meow

---
+++ level 024/home/homework.py
@@ -0,0 +1,30 @@
+# def sum(num1, num2):
+#     print(num1 + num2)
+# sum(2, 5)
+
+
+
+# def num(num):
+#     if num % 2 == 0:
+#         print("this num is even")
+#     else:
+#         print("this num is odd")
+# num(21)
+
+
+
+# def num(num):
+#     print(num**2)
+# num(12)
+
+
+
+# def word(word):
+#     print(word.upper())
+# word("hello")
+
+
+
+# def name(name, last_name):
+#     print(f"hello, my name is {name} {last_name}")
+# name("anton","petrovich")
