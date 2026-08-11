8a70a859c814c12297d1ffeb8f6df6d412bea8d1
mariamtkemaladze <mariamtkemaladze1234@gmail.com>
2026-08-11 16:34:07 +0400

    meow

---
+++ level 028/class/classwork.py
@@ -0,0 +1,16 @@
+# def even_or_odd(number):
+#     if number % 2 == 0:
+#         return "Even"
+#     else:
+#         return "Odd"
+    
+    
+#     def basic_op(operator, value1, value2): 
+#     if operator == "+":
+#          return value1 + value2
+#     elif operator == "-":
+#         return value1 - value2
+#     elif operator == "*":
+#         return value1 * value2
+#     else:
+#         return value1 / value2
---
+++ level 028/home/homework.py
@@ -0,0 +1,38 @@
+# def simple_multiplication(number):
+#     if number % 2 == 0:
+#         return number * 8
+#     else:
+#         return number * 9
+
+
+
+
+
+
+# def is_even(n): 
+#     return n%2 == 0
+
+
+
+
+# def basic_op(operator, value1, value2): 
+#     if operator == "+":
+#          return value1 + value2
+#     elif operator == "-":
+#         return value1 - value2
+#     elif operator == "*":
+#         return value1 * value2
+#     else:
+#         return value1 / value2
+
+
+
+
+# def bool_to_word(boolean):
+#     return "Yes" if boolean else "No"
+
+
+# def find_average(numbers):
+#     if numbers == []:
+#         return 0
+#     return sum(numbers) / len(numbers)
